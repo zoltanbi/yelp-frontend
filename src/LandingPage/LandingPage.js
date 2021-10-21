bc8a97525349adb3124c5a363e6d1f0bed7094bd
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-21 16:25:54 -0700

    Moved Searchbar folder and updated imports

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TopNav } from './TopNav/TopNav';
 import logo from '../assets/logo.png';
 import styles from './LandingPage.module.css'
-import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchBar } from './SearchBar/SearchBar';
 
 export function LandingPage() {
     return (
