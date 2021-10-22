18252c61360bc22b97ad53ca0e2b567082159b6d
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-22 12:11:50 -0700

    Add nav bar

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Navbar } from '../Navbar/Navbar';
 
 export function Search() {
     return (
         <div>
-            Search
+            <Navbar/>
         </div>
     );
 }
