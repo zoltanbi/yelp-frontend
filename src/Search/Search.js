11e900eb0382f6eb5fc074b21e9c56abcdccce18
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-22 18:36:10 -0700

    add subnav

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Navbar } from '../Navbar/Navbar';
+import { SubNav } from '../Navbar/SubNav/SubNav';
 
 export function Search() {
     return (
         <div>
             <Navbar/>
+            <SubNav/>
         </div>
     );
 }
