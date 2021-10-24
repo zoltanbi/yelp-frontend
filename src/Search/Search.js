d7717d6bdb69ba2880ef54ba5b079b0a6bb48072
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-24 15:20:33 -0700

    Added SearchResultsSummary

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navbar } from '../Navbar/Navbar';
 import { SubNav } from '../Navbar/SubNav/SubNav';
+import { SearchResultsSummary } from './SearchResultsSummary/SearchResultsSummary';
 
 export function Search() {
     return (
         <div>
             <Navbar/>
             <SubNav/>
+            <SearchResultsSummary/>
         </div>
     );
 }
