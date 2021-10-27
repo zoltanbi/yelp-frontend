753f8258438adf274e54da0caea7e319ab8fa286
Zoltanbi <zoltanbiro001@gmail.com>
2021-10-27 13:02:30 -0700

    Redirect from landing page

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from '../assets/logo.png';
 import styles from './Navbar.module.css';
 import { SearchBar } from '../SearchBar/SearchBar';
+import { Link } from 'react-router-dom';
 
 export function Navbar() {
     return (
         <div className={styles['nav-bar']}>
-            <img className={styles.logo} src={logo} alt='belb logo'/>
+            <Link to='/'><img className={styles.logo} src={logo} alt='belb logo'/></Link>
             <SearchBar small/>
             <button className={`button ${styles['nav-button']}`}>Sign In</button>
             <button className={`button ${styles['nav-button']}`}>Register</button>
